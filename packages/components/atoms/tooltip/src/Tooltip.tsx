ed657e80b5dee3a9cdcc78694c52387facbc8091
difadev28 <difadev@gmail.com>
2025-05-02 21:31:29 +0700

    feat: [000] Update Tooltip positioning version

--- packages/components/atoms/tooltip/src/Tooltip.tsx
+++ packages/components/atoms/tooltip/src/Tooltip.tsx
@@ -1,16 +1,33 @@
-
+import clsx from 'clsx';
 import InfoIcon from './assets/InfoIcon';
-import clsx from 'clsx'
 
-const Tooltip = ({ content, className }: { content: string, className?: {container?:string, text?:string} }) => {
-    return (
-        <div className={clsx('tooltip', className?.container)}>
-            <InfoIcon />
-            <span className={clsx('tooltip_text', className?.text)}>
-                {content}
-            </span>
-        </div>
-    );
+type TooltipProps = {
+  content: string;
+  className?: {
+    container?: string;
+    text?: string;
+  };
+  variant?: 'dark' | 'light';
+  position?: 'top' | 'bottom';
+};
+
+const Tooltip = ({ content, className, variant = 'dark', position="top" }: TooltipProps) => {
+  return (
+    <div className={clsx("tooltip", className?.container)}>
+      <InfoIcon fill={variant=='light' ? "#fff" : '#001A41'} />
+      <span
+        data-testid="dt-tooltip"
+        className={clsx(
+          "tooltip_text",
+          `variant__${variant}`,
+          `position__${position}`,
+          className?.text
+        )}
+      >
+        {content}
+      </span>
+    </div>
+  );
 };
 
 export default Tooltip;
--- src/components/section/SectionTooltip.tsx
+++ src/components/section/SectionTooltip.tsx
@@ -1,21 +1,27 @@
-import { Button } from '@sora-ui-material/button'
-import { Tooltip } from '@sora-ui-material/tooltip'
+import { Tooltip } from '../../../packages/components/atoms/tooltip/src'
 
 const SectionTooltip = () => {
   return (
-    <div className='flex gap-2.5 border border-custom-primary p-3 m-3 flex-col items-start rounded-2xl'>
-      <Button 
-        variant='secondary'
-        className='!border-custom-primary !text-custom-primary'
-      >
-        Tooltip Sample 
-        <Tooltip  
-          content={"Masuk"} 
-          className={{
-            container: "text-custom-primary"
-          }}
-        />
-      </Button>
+    <div className='flex gap-5 items-center justify-center border border-custom-border_default p-7 rounded-lg mt-5'>
+
+      <div className="flex gap-5">
+        <p className="flex items-center">
+          <Tooltip content={"Tes"} position="bottom" />
+          Tes Data Tooltip Bottom
+        </p>
+        <p className="flex items-center">
+          <Tooltip content={"Tes"} />
+          Tes Data Tooltip Top
+        </p>
+        <p className="bg-custom-primary p-3 rounded-xl flex items-center text-white">
+          <Tooltip content={"Tes"} variant="light" />
+          Tes Data Tooltip Top Light
+        </p>
+        <p className="bg-custom-primary p-3 rounded-xl flex items-center text-white">
+          <Tooltip content={"Analisis uji coba dan tidak akan tercatat dalam faktur tagihan."} variant="light" position="bottom" />
+          Tes Data Tooltip Bottom Light
+        </p>
+      </div>
     </div>
   )
 }
